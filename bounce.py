5c55492fbdf1f1d569694d2712f0a0af440173a2
DellaDastur <ddastur@stratfordschools.com>
2017-06-02 11:49:09 -0700

    Add files via upload

--- bounce.py
+++ bounce.py
@@ -11,23 +11,18 @@ canvas.pack()
 tk.update()
 
 class Ball:
-
-    #takes the parameter of the canvas and color of the ball
     def __init__(self, canvas, color):
         self.canvas = canvas
         self.id = canvas.create_oval(10, 10, 25, 25, fill=color)
         self.canvas.move(self.id, 245, 100)
-
+        
     def draw(self):
-        pass
+        self.canvas.move(self.id, 0, -1)
         
 ball = Ball(canvas, 'red')
 
-# this is the main loop of our game
-# we need to add an animation loop to stop the window from closing immediately
-# Central part of the game that controls what it does
-# This loop for the moment just tells tkinter to redraw the screen.
 while 1:
+    ball.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
